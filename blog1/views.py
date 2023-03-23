648d04655e7cb90e997df1afb825bd7a6abe8432
jonatan Sciaky <j.sciaky@libero.it>
2023-03-23 23:00:49 +0100

    finally fixed bug

---
+++ blog1/views.py
@@ -0,0 +1,9 @@
+
+
+from django.utils import timezone
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog1/post_list.html', {'posts': posts})
